fe2b7938a7c362971aa98bc4aaede43852e64f00
Daniel-N98 <Danieldnee@hotmail.com>
2022-09-06 11:31:01 +0100

    6 - PATCH /api/articles/:article_id implementation

--- controllers/app.controller.js
+++ controllers/app.controller.js
@@ -4,6 +4,7 @@ const {
   fetchTopics,
   fetchArticleById,
   fetchUsers,
+  patchArticleById,
 } = require("../models/app.model.js");
 
 exports.fetchTopics = (request, response, next) => {
@@ -31,6 +32,16 @@ exports.fetchUsers = (request, response, next) => {
   fetchUsers()
     .then((users) => {
       response.status(200).send({ users });
+    })
+    .catch((error) => next(error));
+};
+
+exports.patchArticle = (request, response, next) => {
+  const { article_id } = request.params;
+  const { inc_votes } = request.body;
+  patchArticleById(article_id, inc_votes)
+    .then((updated_article) => {
+      response.status(200).send({ updated_article });
     })
     .catch((error) => next(error));
 };
--- models/app.model.js
+++ models/app.model.js
@@ -18,3 +18,25 @@ exports.fetchArticleById = (article_id) => {
 exports.fetchUsers = () => {
   return db.query(`SELECT * FROM users`).then((users) => users.rows);
 };
+
+exports.patchArticleById = async (article_id, votes) => {
+  if (!Number.parseInt(article_id) || !Number.parseInt(votes)) {
+    return Promise.reject({ code: 400, message: "Bad request" });
+  }
+  let currentVotes = await db.query(
+    "SELECT votes FROM articles WHERE article_id = $1",
+    [article_id]
+  );
+  if (currentVotes.rowCount === 0) {
+    return Promise.reject({ code: 404, message: "Article not found" });
+  }
+  currentVotes = currentVotes.rows[0].votes;
+  return db
+    .query(
+      "UPDATE articles SET votes = $1 WHERE article_id = $2 RETURNING *;",
+      [currentVotes + votes, article_id]
+    )
+    .then((article) => {
+      return article.rows[0];
+    });
+};
