4ca1698298d7942d318ab3b1971fcf6c0c8ecea5
Daniel-N98 <Danieldnee@hotmail.com>
2022-09-08 15:24:33 +0100

    16 routers updated to use route

--- routes/article-router.js
+++ routes/article-router.js
@@ -8,9 +8,7 @@ const {
 const articleRouter = require("express").Router();
 
 articleRouter.get("/", fetchArticles);
-articleRouter.get("/:article_id", fetchArticleById);
-articleRouter.patch("/:article_id", patchArticle);
-articleRouter.get("/:article_id/comments", fetchArticleComments);
-articleRouter.post("/:article_id/comments", postArticleComment);
+articleRouter.route('/:article_id').get(fetchArticleById).patch(patchArticle);
+articleRouter.route('/:article_id/comments').get(fetchArticleComments).post(postArticleComment);
 
 module.exports = articleRouter;
